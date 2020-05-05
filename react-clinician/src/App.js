ed76cf4ca55328ff46e6b03c0c72567a2e0c25b2
pjayathissa <p.jayathissa@gmail.com>
2020-05-05 15:51:42 +1200

    lets get af resh start

--- react-clinician/src/App.js
+++ react-clinician/src/App.js
@@ -1,67 +1,26 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom';
-import { withFirebase } from './components/Firebase';
-
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Header from './components/Header/Header';
-import Navigation from './components/Navigation/Navigation';
-import SignInPage from './components/SignIn/index.js';
-import HomePage from './components/Home/Home';
-import * as ROUTES from './constants/routes';
-
-import { AuthUserContext } from './components/Session';
-
-import 'tachyons';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
- 
-    this.state = {
-      authUser: null,
-    };
-  }
-
-  componentDidMount() {
-    this.listener = this.props.firebase.auth.onAuthStateChanged(
-      authUser => {
-      authUser
-        ? this.setState({ authUser })
-        : this.setState({ authUser: null });
-    });
-  }
-
-  // to prevent memory leaks if the component unmounts
-  componentWillUnmount() {
-    this.listener();
-  }
- 
-  render() {
-    console.log(this.state.authUser);
-    return (
-       <AuthUserContext.Provider value={this.state.authUser}>
-      <Router>
-      {authUser =>
-        authUser ? <HomePage /> : <SignInPage />
-      }
-        <div>
-          <Navigation />
- 
-          <hr/>
- 
-          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-          <Route path={ROUTES.HOME} component={HomePage} />
-        </div>
-      </Router>
-      </AuthUserContext.Provider>
-    );
-  }
-
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
- 
-export default withFirebase(App);
+
+export default App;
